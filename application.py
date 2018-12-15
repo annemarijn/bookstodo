b64bda96f8ba3e199584be3127650246335f112c
Anne M. Aarssen <anneaarssen@gmail.com>
2018-12-15 09:46:08 +0000

    remove print statements

--- application.py
+++ application.py
@@ -14,7 +14,6 @@ db = SQL("postgres://abuaoqsgbmmiin:ab4016489152edd9bb6b0cd82b0779ffd00e176affc1
 @app.route("/")
 def index():
     todobooks = db.execute("SELECT * FROM books WHERE STATUS = 'TODO'")
-    print(todobooks)
     return render_template("index.html", todobooks=todobooks)
 
 
