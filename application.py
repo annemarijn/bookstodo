bf7ffbb2c5f2c71e17a9953bdb618e5523c5cbf3
annemarijn <anneaarssen@gmail.com>
2025-01-03 09:09:02 +0100

    now for 12-21

--- application.py
+++ application.py
@@ -44,9 +44,6 @@ def loadbooks():
             db.execute("INSERT INTO books (BOOKINFO) VALUES (:bookinfo)", bookinfo=boekje.replace("Books: ", ""))
 
     # for 2024 could only import 4 pages at a time!
-    scanPage("https://linguistlist.org/issues/?page=9&topic=Books&startdate=12/31/2023")
-    scanPage("https://linguistlist.org/issues/?page=10&topic=Books&startdate=12/31/2023")
-    scanPage("https://linguistlist.org/issues/?page=11&topic=Books&startdate=12/31/2023")
     scanPage("https://linguistlist.org/issues/?page=12&topic=Books&startdate=12/31/2023")
     scanPage("https://linguistlist.org/issues/?page=13&topic=Books&startdate=12/31/2023")
     scanPage("https://linguistlist.org/issues/?page=14&topic=Books&startdate=12/31/2023")
@@ -57,7 +54,6 @@ def loadbooks():
     scanPage("https://linguistlist.org/issues/?page=19&topic=Books&startdate=12/31/2023")
     scanPage("https://linguistlist.org/issues/?page=20&topic=Books&startdate=12/31/2023")
     scanPage("https://linguistlist.org/issues/?page=21&topic=Books&startdate=12/31/2023")
-    scanPage("https://linguistlist.org/issues/?page=22&topic=Books&startdate=12/31/2023")
 
     return redirect("/")
 
